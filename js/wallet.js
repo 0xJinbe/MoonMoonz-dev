b213d033ac79a9b1bc31787eeaf16627cde91c41
claudio-germano <wantuir.germano@gmail.com>
2022-09-14 21:02:56 -0300

    adress

--- js/wallet.js
+++ js/wallet.js
@@ -2,7 +2,7 @@ const provider = new ethers.providers.Web3Provider(window.ethereum);
 const signer = provider.getSigner();
 
 const moonMoonz = new ethers.Contract(
-	ADDRESS.MoonMoonz,
+	ADDRESSES.MoonMoonz,
 	ABIS.MoonMoonz,
 	signer
 );
